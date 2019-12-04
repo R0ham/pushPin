d9059e7dad591a2dc4389d4a3fcb9437ed9f0468
CerulanLumina <CerulanLumina@users.noreply.github.com>
2019-12-04 14:19:18 -0500

    fix posters endpoint

--- app/src/api/posters_get.php
+++ app/src/api/posters_get.php
@@ -1,13 +1,11 @@
 <?php
-include('env.php');
-$con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_DB);
-if (mysqli_connect_errno()) {
-    http_response_code(500);
-    header("Content-Type: text/json");
-    echo(json_encode(array('error' => 'unable to connect to database')));
-    exit();
-}
+require_once 'http_error.php';
+require_once '../db-auth/db_inc.php';
 header("Content-Type: text/json");
-$result = mysqli_query($con, "SELECT * FROM posters");
-$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-echo(json_encode($rows));
+
+$res = $pdo->query('SELECT * FROM posters;');
+if ($res) {
+    echo(json_encode($res->fetchAll()));
+} else {
+    error(500, '{"success": false, "message": "unable to execute query"}');
+}
