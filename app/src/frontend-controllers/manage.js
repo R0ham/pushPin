adcbc9976b6cf94febde71ef1394b0857d3c68c1
Ryan Berlin <ryanaberlin@gmail.com>
2019-12-09 15:37:08 -0500

    Delete and edit done

--- app/src/frontend-controllers/manage.js
+++ app/src/frontend-controllers/manage.js
@@ -28,7 +28,7 @@ function generatePoster(poster) {
                             <h2 class="info poster-date">${poster.event_date}</h2>
                             <h3 class="info poster-takedown">Takedown: ${poster.takedown_date}</h3>
                             <p class="info poster-description">${poster.description}</p>
-                            <button class="edit-poster" onclick="editPoster(this)">edit</button>
+                            <button class="edit-poster" onclick="editMode(this)">edit</button>
                             <button class="delete-poster" onclick="deletePoster(this)">delete</button>
                         </div>
                     </div>`);
@@ -36,8 +36,40 @@ function generatePoster(poster) {
     container.append(post);
 }
 
-function editPoster(e) {
+function editMode(e) {
+    const poster = $(e.parentElement);
+    poster.find('.poster-title').replaceWith(`<input type="text" class="edit-title" placeholder="title" value="${poster.find('.poster-title').text()}"/>`);
+    poster.find('.poster-date').replaceWith(`<label><input type="date" class="edit-date" value="${poster.find('.poster-date').text()}"/> event date</label>`);
+    poster.find('.poster-takedown').replaceWith(`<label><input type="date" class="edit-takedown" value="${poster.find('.poster-takedown').text().substring(10)}"/> takedown date</label>`);
+    poster.find('.poster-description').replaceWith(`<textarea class="edit-description" placeholder="description">${poster.find('.poster-description').text()}</textarea>`);
+    poster.find('.edit-poster').replaceWith(`<button class="save-poster" onclick="savePoster(this)">save</button>`);
+}
 
+function viewMode(p) {
+    const poster = $(p);
+    poster.find('.edit-title').replaceWith(`<h2 class="info poster-title">${poster.find('.edit-title').val()}</h2>`);
+    poster.find('.edit-date').replaceWith(`<h2 class="info poster-date">${poster.find('.edit-date').val()}</h2>`);
+    poster.find('.edit-takedown').replaceWith(`<h3 class="info poster-takedown">Takedown: ${poster.find('.edit-takedown').val()}</h3>`);
+    poster.find('.edit-description').replaceWith(`<p class="info poster-description">${ poster.find('.edit-description').val()}</p>`);
+    poster.find('.save-poster').replaceWith(`<button class="edit-poster" onclick="editMode(this)">edit</button>`);
+}
+
+function savePoster(p) {
+    const poster = $(p.parentElement);
+    const posterData = {};
+
+    posterData.pid = poster.attr('id').substring(7);
+    posterData.title = poster.find('.edit-title').val();
+    posterData.description = poster.find('.edit-description').val();
+    posterData.eventDate = poster.find('.edit-date').val();
+    posterData.takedown = poster.find('.edit-takedown').val();
+
+    $.ajax({
+        url: '/api/edit_poster.php',
+        type: 'PUT',
+        data: posterData,
+        success: () => { viewMode(poster); }
+    });
 }
 
 function deletePoster(e) {
@@ -46,7 +78,7 @@ function deletePoster(e) {
         $.ajax({
             url: '/api/delete_poster.php',
             type: 'DELETE',
-            data: {'id': id},
+            data: {'pid': id},
             success: function() {
                 $(e.parentElement.parentElement).remove();
             }
