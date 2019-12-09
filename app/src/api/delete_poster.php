5a3b9e025854d9f64e6a70f1ab408d0a49c0e969
CerulanLumina <CerulanLumina@users.noreply.github.com>
2019-12-09 14:35:53 -0500

    fix edit/delete

--- app/src/api/delete_poster.php
+++ app/src/api/delete_poster.php
@@ -6,9 +6,13 @@ require_once 'http_error.php';
 require_once '../db-auth/db_inc.php';
 require_once '../db-auth/account_class.php';
 
+$account = new Account();
+
 try {
     if ($account->sessionLogin()) {
         $uid = $account->getUserID();
+    } else {
+        error(403, 'Not logged in');
     }
 } catch (Exception $e) {
     error(500, $e->getMessage());
@@ -17,20 +21,27 @@ try {
 if (!isset($_REQUEST['pid'])) {
     error(400, 'Missing poster id (pid) parameter');
 }
-
-$checkPosterAccQuery = 'SELECT account_id FROM posters WHERE poster_id = :pid;';
-$pq = $pdo->prepare($checkPosterAccQuery);
-$pq->execute(array(':pid' => $_REQUEST['pid']));
+try {
+    $checkPosterAccQuery = 'SELECT account_id FROM posters WHERE poster_id = :pid;';
+    $pq = $pdo->prepare($checkPosterAccQuery);
+    $pq->execute(array(':pid' => $_REQUEST['pid']));
+} catch (Throwable $ex) {
+    error(500, $ex->getMessage());
+}
 $poster = $pq->fetchAll(PDO::FETCH_ASSOC);
 if (count($poster) !== 1)
     error(400, 'Unknown poster id');
-if ($poster['account_id'] !== $uid) {
-    error(403, 'Unauthorized to edit poster');
+$poster = $poster[0];
+if ($poster['account_id'] != $uid) {
+    error(403, 'Unauthorized to edit poster)');
 }
-
-$pq = $pdo->prepare('DELETE FROM posters WHERE poster_id = :pid');
-if (!$pq->execute(array(':pid' => $_REQUEST['pid']))) {
-    error(500, 'Unable to delete poster');
+try {
+    $pq = $pdo->prepare('DELETE FROM posters WHERE poster_id = :pid');
+    if (!$pq->execute(array(':pid' => $_REQUEST['pid']))) {
+        error(500, 'Unable to delete poster');
+    }
+} catch (Throwable $ex) {
+    error(500, $ex->getMessage());
 }
 
 header("Content-Type: text/json");
--- app/src/api/edit_poster.php
+++ app/src/api/edit_poster.php
@@ -4,10 +4,12 @@ session_start();
 require_once 'http_error.php';
 require_once '../db-auth/db_inc.php';
 require_once '../db-auth/account_class.php';
-
+$account = new Account();
 try {
     if ($account->sessionLogin()) {
         $uid = $account->getUserID();
+    } else {
+        error(403, 'Not logged in');
     }
 } catch (Exception $e) {
     error(500, $e->getMessage());
@@ -17,20 +19,21 @@ if (!isset($_REQUEST['pid'])) {
     error(400, 'Missing poster id (pid) parameter');
 }
 
-$checkPosterAccQuery = 'SELECT account_id FROM posters WHERE poster_id = :pid;';
+$checkPosterAccQuery = 'SELECT account_id, title, description, event_date, takedown_date FROM posters WHERE poster_id = :pid;';
 $pq = $pdo->prepare($checkPosterAccQuery);
 $pq->execute(array(':pid' => $_REQUEST['pid']));
 $poster = $pq->fetchAll(PDO::FETCH_ASSOC);
 if (count($poster) !== 1)
     error(400, 'Unknown poster id');
-if ($poster['account_id'] !== $uid) {
+$poster = $poster[0];
+if ($poster['account_id'] != $uid) {
     error(403, 'Unauthorized to edit poster');
 }
 
-$title = $_REQUEST['title'];
-$des = $_REQUEST['description'];
-$eventDate = $_REQUEST['eventDate'];
-$takedown = $_REQUEST['takedown'];
+$title = isset($_REQUEST['title']) ? $_REQUEST['title'] : $poster['title'];
+$des =       isset($_REQUEST['description']) ? $_REQUEST['description'] : $poster['description'];
+$eventDate = isset($_REQUEST['eventDate']) ? $_REQUEST['eventDate'] : $poster['event_date'];
+$takedown =  isset($_REQUEST['takedown']) ? $_REQUEST['takedown'] : $poster['takedown_date'];
 
 $pq = $pdo->prepare('UPDATE posters SET title = :title, description = :des, event_date = :eventDate, takedown_date = :takedown WHERE poster_id = :pid');
 if (!$pq->execute(array(':pid' => $_REQUEST['pid'], ':title' => $title, ':des' => $des, ':eventDate' => $eventDate, ':takedown' => $takedown))) {
